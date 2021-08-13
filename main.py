2e7336615c58134513e4da2454156556640d92ca
locklook <playcat.k5@gmail.com>
2021-08-13 21:41:29 +0300

    Update 0.5

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import discord
 from discord.ext import commands
+from discord.utils import get
 from PIL import Image
-import os
+import json
 
-client = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix="/")
 
 # Region owners
-colonists_regions = []
-nation_regions = []
-brigands_regions = []
-treasury_regions = []
-events_regions = []
+
+
+with open("clans.json", "rb") as file:
+    clans = json.load(file)
+    colonists_regions = clans["colonists_regions"]
+    nation_regions = clans["nation_regions"]
+    brigands_regions = clans["brigands_regions"]
+    treasury_regions = clans["treasury_regions"]
+    events_regions = clans["events_regions"]
+
 
 # Dict of all flag
 flags = {"red": Image.open("flag_red.png"),
@@ -19,91 +25,149 @@ flags = {"red": Image.open("flag_red.png"),
          "chest": Image.open("flag_chest.png")
 }
 
-
 # px - half resolution flag image
-px = 64
+px = 16
 # Regions coordinates
-regions_cords = {
-    "11111": (516 - px, 245 - px),
-    "22222": (170 - px, 195 - px),
-    "33333": (1160 - px, 145 - px),
-    "44444": (900 - px, 260 - px),
-    "55555": (420 - px, 495 - px),
-    "66666": (225 - px, 685 - px),
-    "77777": (765 - px, 490 - px),
-    "88888": (1065 - px, 660 - px),
-    "99999": (1375 - px, 410 - px),
-    "21111": (685 - px, 760 - px),
-    "32222": (1415 - px, 870 - px),
-    "43333": (90 - px, 990 - px),
-    "54444": (445 - px, 980 - px),
-    "65555": (785 - px, 930 - px),
-    "76666": (1030 - px, 1030 - px),
-    "87777": (1175 - px, 1195 - px),
-    "98888": (100 - px, 1185 - px),
-    "12333": (285 - px, 1185 - px),
-    "23133": (550 - px, 1185 - px),
-    "43211": (740 - px, 1100 - px),
-    "78917": (1375 - px, 1350 - px),
-    "98213": (135 - px, 1405 - px),
-    "12312": (480 - px, 1405 - px),
-    "98989": (850 - px, 1350 - px)
+regions_info = {
+    "Shift": (34, 87, 64),
+    "BigOasis": (286, 197, 86),
+    "MushroomMadness": (610, 92, 32),
+    "SnowPeak": (104, 332, 64),
+    "Resonance": (373, 401, 64),
+    "NaturalComplex": (689, 273, 64),
+    "CornerHopelessness": (48, 657, 44),
+    "FieldsOfPerdition": (208, 560, 86),
+    "Melting": (417, 576, 64),
+    "Distance": (661, 630, 64)
 }
+
+
+def updjson():
+    with open('clans.json') as f:
+        data = json.load(f)
+    # update the data dictionary then re-dump it in the file
+    data.update({
+        "colonists_regions": colonists_regions,
+        "nation_regions": nation_regions,
+        "brigands_regions": brigands_regions,
+        "treasury_regions": treasury_regions,
+        "events_regions": events_regions
+    })
+
+    with open('clans.json', 'w') as data_file:
+        json.dump(data, data_file, indent=2)
 
 
 def get_active_regions():
     """Возвращает список кортежев - координаты, и тип команды(тег, метка, имя клана, и т.д.)"""
     active_regions = []
     for regions in colonists_regions:
-        active_regions.append((regions_cords[regions], "blue"))
+        active_regions.append((regions_info[regions], "blue"))
     for regions in brigands_regions:
-        active_regions.append((regions_cords[regions], "red"))
+        active_regions.append((regions_info[regions], "red"))
     for regions in nation_regions:
-        active_regions.append((regions_cords[regions], "green"))
+        active_regions.append((regions_info[regions], "green"))
     for regions in treasury_regions:
-        active_regions.append((regions_cords[regions], "chest"))
+        active_regions.append((regions_info[regions], "chest"))
     return active_regions
 
 
 @client.command()
-async def карта(ctx, *agr):
-    region_map = Image.open("map.png")
+async def map(ctx, *agr):
+    try:
+        await ctx.message.delete()
+    except Exception:
+        pass
+    region_map = Image.open("map_new.png")
     for region in get_active_regions():
-        region_map.paste(flags[region[1]], region[0], flags[region[1]])
+        # Paste flag on map. First img flag. Second - its region pos in map x,y([0][0] = pos, [0][1] flag size)
+        # region[1] = name of the flag,
+        # region [0] = info of the region: [0] - pos x on map, [1] - pos y on map, [2] - size of flag
+        curr_flag = flags[region[1]].resize((region[0][2], region[0][2]))
+        region_map.paste(curr_flag, (region[0][0]-(int(region[0][2]/2)), region[0][1]-(int(region[0][2]/2))), curr_flag)
     region_map.save("map_remake.png")
     await ctx.send(file=discord.File("map_remake.png"))
 
 
 @client.command()
 async def team(ctx):
-    await ctx.send(f"Владения месный: {nation_regions}, владения колонистов: {colonists_regions}, владения разбойников: {brigands_regions}")
+    await ctx.message.delete()
+    if get(ctx.author.roles, name="Region editor"):
+        await ctx.send(f"Владения месный: {nation_regions}, владения колонистов: {colonists_regions}, владения разбойников: {brigands_regions}")
+
+
+def region_is_exists(name):
+    exist = False
+    for region in regions_info.keys():
+        if name == region:
+            exist = True
+            return exist
+    return exist
 
 
 @client.command()
 async def update(ctx, team, *agr):
-    clans = [colonists_regions, nation_regions, brigands_regions]
-    teams = {"red": brigands_regions,
-            "blue": colonists_regions,
-            "green": nation_regions
-    }
-    clans.remove(teams[team])
-    teams[team].append(str(agr[0]))
-    for region in clans:
-        try:
-            region.remove(agr[0])
-        except ValueError:
-            pass
-    await ctx.send(f"Вы добавили во владения {team}, регион {agr[0]}")
+    await ctx.message.delete()
+    if get(ctx.author.roles, name="Region editor"):
+        if region_is_exists(agr[0]):
+            clans_to_clear = [colonists_regions, nation_regions, brigands_regions]
+            teams = {"red": brigands_regions,
+                    "blue": colonists_regions,
+                    "green": nation_regions
+            }
+            # Delete current clan in check_to_delete_region list (clans_to_clear)
+            clans_to_clear.remove(teams[team])
+            # Give region to current clan
+
+            # Check region in another clans, and delete it if exists
+            for region in clans_to_clear:
+                try:
+                    region.remove(agr[0])
+                except ValueError:
+                    pass
+            check_region_exists = 0
+            for region in teams[team]:
+                if region == agr[0]:
+                    check_region_exists = 1
+                    break
+            if not check_region_exists:
+                teams[team].append(str(agr[0]))
+                updjson()
+                await ctx.send(f"{ctx.author.mention}, вы добавили во владения {team}, регион {agr[0]}")
+                await map(ctx)
+            else:
+                await ctx.send(f"{ctx.author.mention}, такой регион уже имееться во владениях этого клана.")
+        else:
+            await ctx.send(f"{ctx.author.mention}, вы не правильно написали имя региона!")
+    else:
+        await ctx.send(f"{ctx.author.mention}, у вас не прав на изменения регионов у кланов!")
 
 
 @client.command()
 async def delete(ctx, team, *agr):
-    teams = {"red": brigands_regions,
-             "blue": colonists_regions,
-             "green": nation_regions
-             }
-    teams[team].remove(agr[0])
-    await ctx.send(f"Вы удалили из владений {team}, регион {agr[0]}")
+    await ctx.channel.purge(limit=1)
+    if get(ctx.author.roles, name="Region editor"):
+        if region_is_exists(agr[0]):
+            teams = {"red": brigands_regions,
+                     "blue": colonists_regions,
+                     "green": nation_regions,
+                     }
+            teams[team].remove(agr[0])
+            updjson()
+            await ctx.send(f"{ctx.author.mention}, вы удалили из владений {team}, регион {agr[0]}")
+        else:
+            await ctx.send(f"{ctx.author.mention}, вы не правильно написали имя региона!")
+    else:
+        await ctx.send(f"{ctx.author.mention}, у вас не прав на изменения регионов у кланов!")
+
+
+
+
+@client.command()
+async def addchest(ctx, *agr):
+    if get(ctx.author.roles, name="Region editor"):
+        treasury_regions.append(agr[0])
+        updjson()
 
 
 client.run("ODcxNDc5OTIzMTMzNTI2MDY3.YQb67A.e6RIf9gosZWXufZE1I88eDJ6JwE")
--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-a = ["4214"]
-a.append(1212)
-print(a)
+import json
+with open('testj.json') as data_file:
+    data = json.load(data_file)
+    # update the data dictionary then re-dump it in the file
+    data.update({
+        "colonists_regions": ["66666", "77777"],
+        "nation_regions": [],
+        "brigands_regions": [],
+        "treasury_regions": [],
+        "events_regions": []
+    })
+
+with open('testj.json', 'w') as data_file:
+    json.dump(data, data_file, indent=2)
